fe3cf48e4b012106915dd517e34fa8ae9009291c
salsabilpl <90193321+salsabilpl@users.noreply.github.com>
2024-02-13 05:04:23 +0700

    Update deploy_streamlit.py

--- deploy_streamlit.py
+++ deploy_streamlit.py
@@ -89,8 +89,8 @@ tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
 with tab1 :
     st.sidebar.header("**User Input** Sidebar")
 
-    age = st.sidebar.number_input(label=":violet[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
-    st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
+    age = st.sidebar.number_input(label=":violet[**Age**]", min_value=df_final['Age'].min(), max_value=df_final['Age'].max())
+    st.sidebar.write(f":orange[Min] value: :orange[**{df_final['Age'].min()}**], :red[Max] value: :red[**{df_final['Age'].max()}**]")
     st.sidebar.write("")
 
     gender_sb = st.sidebar.selectbox(label=":blue[**Gender**]", options=["Male", "Female"])
